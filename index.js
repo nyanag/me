22c3a71fa51ce7e2d054789bdc6681b832cb77bf
Ananya <ananyaganesh01@gmail.com>
2020-04-27 22:49:23 +0530

    Nav commit

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ about.addEventListener('click', function(){
     content.style.display = "none";
     intro.style.display = "block";
 })
+
+work.addEventListener('click',function(){
+    intro.style.display = "none";
+    content.style.display = "block";
+    content.innerHTML = "Coming Soon :)"
+})
+
+donate.addEventListener('click',function(){
+    intro.style.display = "none";
+    content.style.display = "block";
+    content.innerHTML = "Coming Soon :)"
+})
