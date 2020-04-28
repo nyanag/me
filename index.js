b64d03af62c97a1e69c189cd4a2980062e9455a3
Ananya <ananyaganesh01@gmail.com>
2020-04-28 17:10:12 +0530

    Added work

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 blog.addEventListener('click',function(){
     intro.style.display = "none";
     content.style.display = "block";
-    content.innerHTML = "Coming Soon :)"
+    work_div.style.display = "none"
+
+    content.innerHTML = "Coming Soon!"
 })
 
 about.addEventListener('click', function(){
     content.style.display = "none";
     intro.style.display = "block";
+    work_div.style.display = "none"
 })
 
 work.addEventListener('click',function(){
     intro.style.display = "none";
-    content.style.display = "block";
-    content.innerHTML = "Coming Soon :)"
+    content.style.display = "none";
+    work_div.style.display = "block"
 })
 
-donate.addEventListener('click',function(){
-    intro.style.display = "none";
-    content.style.display = "block";
-    content.innerHTML = "Coming Soon :)"
-})
+// donate.addEventListener('click',function(){
+//     intro.style.display = "none";
+//     content.style.display = "block";
+//     content.innerHTML = "Coming Soon :)"
+// })
